f85c3d8a934857f4a9a5587032b14a59827bbd95
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-06-04 11:00:44 +0200

    initial push backend

---
+++ backend/src/main/java/nl/itvitae/buildachar/BuildACharApplication.java
@@ -0,0 +1,13 @@
+package nl.itvitae.buildachar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildACharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildACharApplication.class, args);
+	}
+
+}
