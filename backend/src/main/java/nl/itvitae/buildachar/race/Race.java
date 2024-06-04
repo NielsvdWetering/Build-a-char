2faa530fe88d16a35baa0645926cbb7d778a0f35
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-04 13:55:06 +0200

    Update Race.java

--- backend/src/main/java/nl/itvitae/buildachar/race/Race.java
+++ backend/src/main/java/nl/itvitae/buildachar/race/Race.java
@@ -1,9 +1,36 @@
 package nl.itvitae.buildachar.race;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Race {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(length=100)
+    private String name;
     private final Integer baseStrength;
+    private final Integer baseDexterity;
+    private final Integer baseConstitution;
+    private final Integer baseIntelligence;
+    private final Integer baseWisdom;
+    private final Integer baseCharisma;
 
+    //@OneToMany
+    //private final Character character;
+
+    public Race(Integer baseStrength, Integer baseDexterity, Integer baseConstitution, Integer baseIntelligence, Integer baseWisdom, Integer baseCharisma) {
+        this.baseStrength = baseStrength;
+        this.baseDexterity = baseDexterity;
+        this.baseConstitution = baseConstitution;
+        this.baseIntelligence = baseIntelligence;
+        this.baseWisdom = baseWisdom;
+        this.baseCharisma = baseCharisma;
+    }
 }
