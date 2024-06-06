cc740bdb1e640d076e971687e253af74999bf5e6
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-06-06 12:01:56 +0200

    fix frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import Creator from "./components/creator/creator";
 import Characters from "./components/characters/characters";
-
 
 import { Routes, Route } from "react-router-dom";
 
