4c709611ae98a69e35279c9fa63dcb07583248b0
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-06 11:37:49 +0200

    fixed my error

--- backend/src/main/java/nl/itvitae/buildachar/race/RaceController.java
+++ backend/src/main/java/nl/itvitae/buildachar/race/RaceController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(ControllerRoutes.CHARACTER_ROUTE)
+@RequestMapping(ControllerRoutes.RACE_ROUTE)
 @CrossOrigin(origins = {"${app-cors}"})
-public class RaceController {
-
-}
+public class RaceController {}
