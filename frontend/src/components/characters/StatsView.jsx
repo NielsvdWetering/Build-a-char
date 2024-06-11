4522fee59544dfb711a262db31ed7d3b002f7e7c
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-11 15:00:33 +0200

    edited key

--- frontend/src/components/characters/StatsView.jsx
+++ frontend/src/components/characters/StatsView.jsx
@@ -4,12 +4,8 @@ import { IndividualStatView } from "./IndividualStatView";
 export const StatsView = ({ stats }) => {
   return (
     <div className="grid aspect-video w-full grid-cols-3 gap-5 rounded-md bg-secondary p-6">
-      {Object.keys(stats).map((key) => (
-        <IndividualStatView
-          label={key}
-          stat={stats[key]}
-          key={`${key + stats}`} // kom ik op terug!! fix me!! forget me not!!
-        />
+      {Object.keys(stats).map((key, i) => (
+        <IndividualStatView label={key} stat={stats[key]} key={`${key + i}`} />
       ))}
     </div>
   );
