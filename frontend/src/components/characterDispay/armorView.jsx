ae99831c0fac2e53d5c0ef2aeda84504a00407b3
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-11 16:55:56 +0200

    edited styling and sorted armor pieces

--- frontend/src/components/characterDispay/armorView.jsx
+++ frontend/src/components/characterDispay/armorView.jsx
@@ -2,13 +2,32 @@ import React from "react";
 
 export const ArmorView = ({ armorList }) => {
   console.log("armorlist", armorList);
+  const armorOrder = ["HEAD", "TORSO", "HANDS", "LEGS", "FEET"];
   return (
-    <ul>
-      {armorList.map((armor) => (
-        <li key={armor.id}>
-          <span>{armor.type}</span>
-        </li>
-      ))}
+    <ul className="aspect-video rounded-md bg-secondary p-5">
+      {armorList
+        .sort((a, b) => {
+          return (
+            armorOrder.indexOf(a.armorType) - armorOrder.indexOf(b.armorType)
+          );
+        })
+        .map((armor) => (
+          <li
+            key={armor.id}
+            className="grid-rows grid h-1/5 grid-cols-3 items-center gap-2 text-secondary-content"
+          >
+            <div className="grid grid-cols-2">
+              <span className="capitalize">
+                {armor.armorType.toLowerCase()}
+              </span>
+              <span className="capitalize">
+                {armor.armorClass.toLowerCase()}
+              </span>
+            </div>
+            <span>{armor.name}</span>
+            <span>{armor.defence}</span>
+          </li>
+        ))}
     </ul>
   );
 };
