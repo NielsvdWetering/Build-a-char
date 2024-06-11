d61b178177c2a8c53dd653fb995698f0f6672dd2
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-06-11 14:27:18 +0200

    created custom color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,19 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["autumn"],
+    themes: [
+      {
+        buildachar: {
+          primary: "#989E80",
+          "primary-content": "#403D3A",
+          secondary: "#989E80",
+          "secondary-content": "#403D3A",
+          accent: "#DCE4C8",
+          "accent-content": "#605B56",
+          neutral: "",
+          "base-100": "#605B56",
+        },
+      },
+    ],
   },
 };
