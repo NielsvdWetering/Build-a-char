d46dc484798966d28e9bc5210e2822bad846be21
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-11 10:46:41 +0200

    Update DisplayField.jsx

--- frontend/src/components/characters/DisplayField.jsx
+++ frontend/src/components/characters/DisplayField.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const DisplayField = ({ label, content }) => {
   return (
-    <>
-      <h6 className="">{label}</h6>
-      <p className="input input-secondary">{content}</p>
-    </>
+    <div className="input input-secondary">
+      <span className="">{label}</span>
+      <span>{content}</span>
+    </div>
   );
 };
