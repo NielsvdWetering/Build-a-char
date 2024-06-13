ba2412610b84605186469f17fe891e6cefe2c46d
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-13 11:25:03 +0200

    removed error component

--- frontend/src/components/characterDispay/subcomponents/characterDescription.jsx
+++ frontend/src/components/characterDispay/subcomponents/characterDescription.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { Error } from "../../generic/error";
 
 export const CharacterDescription = ({ description }) => {
-  if (!description) return <Error msg={"Could not retrieve description"} />;
   return (
-    <div className="shadow-custom-dark h-full w-full rounded-md bg-primary p-4">
+    <div className="h-full w-full rounded-md bg-primary p-4 shadow-custom-dark">
       <span className="text-primary-content">{description}</span>
     </div>
   );
