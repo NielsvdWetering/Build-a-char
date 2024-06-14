4d18803a7a9eda0ed0a76fcd408e30baf1550add
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-14 11:47:49 +0200

    adjusted img height to fit parent

--- frontend/src/components/characterDisplay/subcomponents/CharacterImage.jsx
+++ frontend/src/components/characterDisplay/subcomponents/CharacterImage.jsx
@@ -5,7 +5,11 @@ export const CharacterImage = ({ image }) => {
   return (
     <div className="flex aspect-video w-full items-center justify-center rounded-md bg-accent p-3 text-accent-content shadow-custom-dark">
       <div>
-        <img src={image ? image : Placeholder} alt="profile" />
+        <img
+          src={image ? image : Placeholder}
+          alt="profile"
+          className="max-h-60"
+        />
       </div>
     </div>
   );
