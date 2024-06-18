e807ce1ed63eb06215eff019b1c5e94ac68b612a
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-06-18 15:33:40 +0200

    fixed dark theme issues

--- frontend/src/components/editor/subcomponents/InputGroup.jsx
+++ frontend/src/components/editor/subcomponents/InputGroup.jsx
@@ -1,6 +1,6 @@
 export default function InputGroup({ children, title }) {
   return (
-    <div className="card card-body glass bg-secondary shadow-lg">
+    <div className="card card-body glass bg-primary shadow-lg">
       {title && <div className="card-title">{title}</div>}
       {children}
     </div>
