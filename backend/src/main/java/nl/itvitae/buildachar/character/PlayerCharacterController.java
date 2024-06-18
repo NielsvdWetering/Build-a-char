1f740e46b20d6d76a4528be3146e5d5d9ef812da
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-18 12:58:33 +0200

    refactor

--- backend/src/main/java/nl/itvitae/buildachar/character/PlayerCharacterController.java
+++ backend/src/main/java/nl/itvitae/buildachar/character/PlayerCharacterController.java
@@ -3,6 +3,7 @@ package nl.itvitae.buildachar.character;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import nl.itvitae.buildachar.ControllerRoutes;
@@ -165,11 +166,16 @@ public class PlayerCharacterController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<PlayerCharacterGetDTO> getById(@PathVariable UUID id) {
-    return playerCharacterService
-        .getById(id)
-        .map(PlayerCharacterGetDTO::from)
-        .map(ResponseEntity::ok)
-        .orElseThrow(() -> new RestException(HttpStatus.NOT_FOUND));
+  public ResponseEntity<PlayerCharacterGetDTO> getById(
+      @PathVariable UUID id, Authentication authentication) {
+    Optional<User> user =
+        Optional.ofNullable(authentication).map(auth -> (User) auth.getPrincipal());
+    PlayerCharacter character =
+        playerCharacterService
+            .getById(id)
+            .orElseThrow(() -> new RestException(HttpStatus.NOT_FOUND));
+    boolean isOwned =
+        user.map(value -> value.getId().equals(character.getUser().getId())).orElse(false);
+    return ResponseEntity.ok(PlayerCharacterGetDTO.from(character, isOwned));
   }
 }
