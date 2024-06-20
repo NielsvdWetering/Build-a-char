9c8d1a23d04f11dd146e6cb73ffe538498fbf262
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-20 21:55:57 +0200

    removed unnecesary derived queries

--- backend/src/main/java/nl/itvitae/buildachar/character/PlayerCharacterRepository.java
+++ backend/src/main/java/nl/itvitae/buildachar/character/PlayerCharacterRepository.java
@@ -4,13 +4,10 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import nl.itvitae.buildachar.characterclass.CharacterClass;
 import nl.itvitae.buildachar.race.Race;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 // TODO add pageable!
 
@@ -18,19 +15,9 @@ import org.springframework.data.repository.query.Param;
 public interface PlayerCharacterRepository
     extends JpaRepository<PlayerCharacter, UUID>, JpaSpecificationExecutor<PlayerCharacter> {
 
-  Set<PlayerCharacter> findByUser_id(UUID id);
-
   Set<PlayerCharacter> findByRace(Race race);
 
-  Set<PlayerCharacter> findByCharacterClass(CharacterClass characterClass);
-
-  @Query(
-      "SELECT pc FROM PlayerCharacter pc WHERE pc.race IN :races AND pc.characterClass IN :classes")
-  Set<PlayerCharacter> findByRaceAndClass(
-      @Param("races") Set<Race> races, @Param("classes") Set<CharacterClass> classes);
-
   Set<PlayerCharacter> findByNameContainingIgnoreCase(String param);
-
-  // moet list zijn.
+  
   List<PlayerCharacter> findAll(Specification<PlayerCharacter> specification);
 }
