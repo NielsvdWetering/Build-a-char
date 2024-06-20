d2b2c152053a1aed85103ec5f55d1cf89bb8a2aa
Laila <91194082+Lailaismyname@users.noreply.github.com>
2024-06-20 08:46:31 +0200

    minor styling of unused component

--- frontend/src/components/characters/subcomponents/Filter.jsx
+++ frontend/src/components/characters/subcomponents/Filter.jsx
@@ -2,7 +2,7 @@ import { RadioButton } from "./RadioButton";
 
 export const Filter = ({ handleOnCheck }) => {
   return (
-    <div className="space-evenly grid w-fit rounded-lg border-2 p-4">
+    <div className="space-evenly my-8 grid w-fit rounded-lg border-4 border-double border-primary p-8">
       <form>
         <RadioButton label={"All characters"} handleOnCheck={handleOnCheck} />
         <RadioButton label={"My characters"} handleOnCheck={handleOnCheck} />
