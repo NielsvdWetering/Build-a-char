03f8226f27af5cc8a0e8f55789cb119c2803bb76
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-06-21 14:39:48 +0200

    updated image placement

--- frontend/src/components/characterDisplay/subcomponents/CharacterImage.jsx
+++ frontend/src/components/characterDisplay/subcomponents/CharacterImage.jsx
@@ -4,18 +4,16 @@ import { useState, useEffect } from "react";
 
 export const CharacterImage = ({ pictureId }) => {
   return (
-    <div className="flex aspect-video w-full items-center justify-center rounded-md bg-accent p-3 text-accent-content shadow-custom-dark">
-      <div>
-        <img
-          src={
-            pictureId != null
-              ? `http://localhost:8080/api/v1/characters/image/${pictureId}`
-              : Placeholder
-          }
-          alt="profile"
-          className="max-h-60"
-        />
-      </div>
+    <div className="flex aspect-video w-full items-center justify-center rounded-md bg-accent text-accent-content shadow-custom-dark">
+      <img
+        src={
+          pictureId != null
+            ? `http://localhost:8080/api/v1/characters/image/${pictureId}`
+            : Placeholder
+        }
+        alt="profile"
+        className="aspect-video w-full rounded-md border border-solid border-gray-800"
+      />
     </div>
   );
 };
