73d5321f302a3dffa2473e9ccad3411463b76d84
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-06-21 15:10:09 +0200

    Updated home back to temp homepage

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -4,31 +4,9 @@ import { useState, useEffect } from "react";
 import { useApi } from "../../hooks";
 
 export default function Home() {
-  const [img, setImg] = useState();
-  const [loading, setLoading] = useState();
-
-  useEffect(() => {
-    fetchImage();
-  }, []);
-
-  const fetchImage = async () => {
-    setLoading(true);
-    const res = await fetch(
-      "http://localhost:8080/api/v1/characters/image/f3471312-1c68-4597-ba19-98bb92e22fb7",
-    );
-    const imageBlob = await res.blob();
-    const imageObjectURL = URL.createObjectURL(imageBlob);
-    setImg(imageObjectURL);
-    console.log(img);
-    setLoading(false);
-  };
-
-  if (loading) {
-    return <div>loading</div>;
-  }
   return (
     <>
-      <img src={img} alt="" />
+      <CharacterCreator />
     </>
   );
 }
