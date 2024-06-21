68ed0d856f42d91b4431ccd713b690a4430512e2
Niels <46904611+NielsvdWetering@users.noreply.github.com>
2024-06-21 16:00:04 +0200

    implemented pictures

--- frontend/src/components/characters/subcomponents/Card.jsx
+++ frontend/src/components/characters/subcomponents/Card.jsx
@@ -1,15 +1,23 @@
+import { CharacterImage } from "../../characterDisplay/subcomponents/CharacterImage";
 import { DisplayField } from "./DisplayField";
 import img from "/cutebear.png";
 export const Card = ({ character, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="cursor-pointer rounded bg-primary p-2 shadow-custom-dark hover:bg-primary-hover"
+      className="hover:bg-primary-hover cursor-pointer rounded bg-primary p-2 shadow-custom-dark"
     >
       {character && (
         <div className="flex flex-row">
           <div className="w-52 p-4">
-            <img src={img} className="rounded-lg" />
+            <img
+              src={
+                character.characterPictureId != null
+                  ? `http://localhost:8080/api/v1/characters/image/${character.characterPictureId}`
+                  : img
+              }
+              className="rounded-lg"
+            />
           </div>
           <div className="flex w-2/3 flex-col justify-between py-6 pl-4 pr-2">
             <DisplayField content={character.name} />
